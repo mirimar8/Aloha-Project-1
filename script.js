5b1925e20070a74645176895028d7d567379365d
Miri <mirimarkovitzbl@gmail.com>
2019-04-30 10:38:48 -0700

    Remove extraneous code and comments from files and delete blank spaces

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('.nav-list li a');
 
     navLinks.forEach(function(element) {
-        // console.log(element);
         element.addEventListener('click', function(event) {
             event.preventDefault();
             document.querySelector(event.target.hash).scrollIntoView({ 
@@ -28,69 +27,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
         document.querySelector('.subscribe').addEventListener('click', subscribe);
 
-        
-    
-
-
-    // forms['form']['email']
-
-
-
-
-    // const about = document.getElementById('about');
-    // const shop = document.getElementById('shop');
-    // const featured = document.getElementById('featured');
-    // const updates = document.getElementById('updates');
-
-    // about.addEventListener('click', function(event) {
-    //     event.preventDefault();
-    //     document.querySelector('.about-section').scrollIntoView({ 
-    //         behavior: 'smooth' 
-    //       })
-    //       console.log('about', event.target.hash);
-    // });
-
-    // shop.addEventListener('click', function(event) {
-    //     event.preventDefault();
-    //     document.querySelector('.products-section').scrollIntoView({ 
-    //         behavior: 'smooth' 
-    //       })
-    //       console.log('shop', event);
-
-    // });
-
-
-    // featured.addEventListener('click', function(event) {
-    //     event.preventDefault();
-    //     document.querySelector('.featured-section').scrollIntoView({ 
-    //         behavior: 'smooth' 
-    //       })
-    //       console.log('featured', event);
-
-    // });
-
-
-    // updates.addEventListener('click', function(event) {
-    //     event.preventDefault();
-    //     document.querySelector('.updates-section').scrollIntoView({ 
-    //         behavior: 'smooth' 
-    //       })
-    //       console.log('updates', event);
-
-    // });
-
-
-    
-
-    
-    
-    
-    
-    
-    
-
-
-
-
-    
 });
